fde5485dc826b038bbe3f0ac7c4bd50ac384aaf4
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-31 17:07:45 -0700

    put data onto game_list

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -1,6 +1,24 @@
+const async = require('async');
 //Model
 const gameSchema = require('../models/game');
+const developerSchema = require('../models/developer');
 
 exports.index = (req, res) => {
-    res.render('./game/game_list');
+
+    async.parallel({
+        games(callback){
+            gameSchema.where("name")
+            .populate("developer")
+            .populate("genre")
+            .exec(callback);
+        },
+    }, (err, results) => {
+        if (err) throw err;
+
+        console.log(results);
+        res.render('./game/game_list', {
+            games: results.games
+        });
+
+    })
 }
