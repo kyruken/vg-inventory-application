307c0ac8aa52fbdd7353cf3cf3baf11acb92549c
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-05 16:12:10 -0700

    add all route controls

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -4,3 +4,30 @@ const genreSchema = require('../models/genre');
 exports.index = (req, res) => {
     res.render('./genre/genre_list');
 }
+
+exports.genre_detail_get = (req, res) => {
+
+}
+
+exports.genre_form_get = (req, res) => {
+}
+
+exports.genre_form_post = (req, res) => {
+
+}
+
+exports.genre_delete_get = (req, res) => {
+
+}
+
+exports.genre_delete_post = (req, res) => {
+
+}
+
+exports.genre_update_get = (req, res, next) => {
+
+}
+
+exports.genre_update_post = (req, res) => {
+    
+}
