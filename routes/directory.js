4a60802f1449703e707fd1b90ee1917e79e3e987
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-05 16:09:10 -0700

    add genre routes to router

--- routes/directory.js
+++ routes/directory.js
@@ -20,7 +20,7 @@ router.post('/game_delete/:id', gameController.game_delete_post);
 router.get('/game_update/:id', gameController.game_update_get);
 router.post('/game_update/:id', gameController.game_update_post);
 
-
+//developer routes
 router.get('/developers', developerController.index);
 router.get('/developer/:id', developerController.developer_detail_get);
 
@@ -33,7 +33,18 @@ router.post('/developer_delete/:id', developerController.developer_delete_post);
 router.get('/developer_update/:id', developerController.developer_update_get);
 router.post('/developer_update/:id', developerController.developer_update_post);
 
+//genre routes
 router.get('/genres', genreController.index);
+router.get('/genre/:id', genreController.genre_detail_get);
+
+router.get('/genre_form/create', genreController.genre_form_get);
+router.post('/genre_form/create', genreController.genre_form_post);
+
+router.get('/genre_delete/:id', genreController.genre_delete_get);
+router.post('/genre_delete/:id', genreController.genre_delete_post);
+
+router.get('/genre_update/:id', genreController.genre_update_get);
+router.post('/genre_update/:id', genreController.genre_update_post);
 
 
 module.exports = router;
