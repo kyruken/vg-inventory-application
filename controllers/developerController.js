7b71da41e6fd7721986e6f98b171cb1defb8059a
Avery Kyle Arenas <kyruken@gmail.com>
2022-11-04 17:15:26 -0700

    add modules and create developer_detail_get

--- controllers/developerController.js
+++ controllers/developerController.js
@@ -1,7 +1,36 @@
+const async = require('async');
+const developer = require('../models/developer');
+
 //Model
 const Developer = require('../models/developer');
+const Game = require('../models/game');
 
 exports.index = (req, res) => {
+    Developer.find()
+        .exec((err, developers) => {
+            res.render('./developer/developer_list', { developers });
 
-    res.render('./developer/developer_list');
+        })
+}
+
+exports.developer_detail_get = (req, res) => {
+    async.parallel(
+        {
+            games(callback) {
+                Game.where("developer")
+                    .equals(req.params.id)
+                    .where("title")
+                    .exec(callback);
+            },
+            developer(callback) {
+                Developer.findById(req.params.id)
+                .exec(callback);
+            }
+        }, (err, data) => {
+            console.log(data);
+            res.render('./developer/developer_detail', { 
+                games: data.games,
+                developer: data.developer
+             });
+        })
 }
